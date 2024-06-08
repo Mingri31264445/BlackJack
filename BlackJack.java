43cb66818183c06ee2018c37d2c9e4cfb26455d9
miscontroi <tsun.an.yang@gmail.com>
2024-06-09 04:11:57 +0800

    (master): debug後成果(暫定終版)

--- BlackJack.java
+++ BlackJack.java
@@ -118,150 +118,151 @@ public class BlackJack {
         //強迫開始的迴圈
         int open;
         int rounds = 0;
-        boolean playGame = true;
-
+        boolean playGame = false;
         do{
-            System.out.println("開始遊戲!:1");
-            System.out.println("退出遊戲:2");//結果輸出放到open
-            //使用 while 迴圈檢查輸入是否為整數
-            while (!sc.hasNextInt()) {
-                System.out.println("輸入錯誤,請輸入1或2:");
-                sc.next(); //這可以清除無效的輸入
-            } 
-            open = sc.nextInt();
+            do{
+                System.out.println("開始遊戲!:1");
+                System.out.println("退出遊戲:2");//結果輸出放到open
+                //使用 while 迴圈檢查輸入是否為整數
+                while (!sc.hasNextInt()) {
+                    System.out.println("輸入錯誤,請輸入1或2:");
+                    sc.next(); //這可以清除無效的輸入
+                } 
+                open = sc.nextInt();
+                    
+                if(open == 1){//當open = 1開始遊戲
                 
-            if(open == 1){//當open = 1開始遊戲
-            
-            //用來重製第二輪莊家、玩家的點數(point)和是否爆牌(gameOver)
-            dealer.reset();
-            for (int i=0;i<player.length;i++) {
-                player[i].reset();
-            }
+                //用來重製第二輪莊家、玩家的點數(point)和是否爆牌(gameOver)
+                dealer.reset();
+                for (int i=0;i<player.length;i++) {
+                    player[i].reset();
+                }
 
-            dealer.showOneCard();//莊家顯示其中一張牌
+                dealer.showOneCard();//莊家顯示其中一張牌
 
-            //玩家回合開始
-            for(int i=0;i<player.length;i++){
-                System.out.println();
-                System.out.println("玩家"+(i+1)+"的回合:");
-                player[i].Licensing();//呼叫開局方法
+                //玩家回合開始
+                for(int i=0;i<player.length;i++){
+                    System.out.println();
+                    System.out.println("玩家"+(i+1)+"的回合:");
+                    player[i].Licensing();//呼叫開局方法
 
 
-                int get;//在do-while外宣告while中才可以使用
-                do{
-                    System.out.println("");
-                    System.out.println("請問要再來一張牌嗎?");
-                    System.out.println("要:1");
-                    System.out.println("不要:2");
-                    while (!sc.hasNextInt()) {
-                        System.out.println("輸入錯誤,請輸入1或2:");
-                        sc.next();
-                    }
-                    get = sc.nextInt();
+                    int get;//在do-while外宣告while中才可以使用
+                    do{
+                        System.out.println("");
+                        System.out.println("請問要再來一張牌嗎?");
+                        System.out.println("要:1");
+                        System.out.println("不要:2");
+                        while (!sc.hasNextInt()) {
+                            System.out.println("輸入錯誤,請輸入1或2:");
+                            sc.next();
+                        }
+                        get = sc.nextInt();
+                    
+                        if(get == 1){//玩家要牌
+                            System.out.print("玩家"+(i+1));
+                            player[i].askForCard();
+                            player[i].show();
+                        }
+                        else if (get == 2) {//玩家不要牌
+                            System.out.println("遊戲結束，最終點數是: " + player[i].point);
+                        }
+                        else{
+                            System.out.println("輸入錯誤,請輸入1或2:");
+                        }
+
+                    }while (get !=2 && !player[i].gameOver);//爆牌自動結束
+
+                }
                 
-                    if(get == 1){//玩家要牌
-                        System.out.print("玩家"+(i+1));
-                        player[i].askForCard();
-                        player[i].show();
-                    }
-                    else if (get == 2) {//玩家不要牌
-                        System.out.println("遊戲結束，最終點數是: " + player[i].point);
-                    }
-                    else{
-                        System.out.println("輸入錯誤,請輸入1或2:");
-                    }
+                //玩家都結束之後，莊家開始
+                System.out.println("");
+                System.out.println("莊家的回合:");
+                dealer.play();
 
-                }while (get !=2 && !player[i].gameOver);//爆牌自動結束
-
-            }
-            
-            //玩家都結束之後，莊家開始
-            System.out.println("");
-            System.out.println("莊家的回合:");
-            dealer.play();
-
-            for(int j=0;j<player.length;j++){
-                //判斷輸贏
-                if (player[j].gameOver) {
-                    System.out.println("");
-                    System.out.println("玩家"+(j+1)+"爆牌，莊家勝利!");
-                    player[j].score -= 2;
-                }
-                else if (dealer.gameOver) {
-                    System.out.println("");
-                    System.out.println("莊家爆牌，玩家"+(j+1)+"勝利!");
-                    player[j].score += 2;
-                }
-                else {
-                    if (player[j].point > dealer.point) {
+                for(int j=0;j<player.length;j++){
+                    //判斷輸贏
+                    if (player[j].gameOver) {
                         System.out.println("");
-                        System.out.println("玩家"+(j+1)+"勝利!");
-                        player[j].score += 2;
-                    }
-                    else if (player[j].point < dealer.point) {
-                        System.out.println("");
-                        System.out.println("莊家勝利!");
+                        System.out.println("玩家"+(j+1)+"爆牌，莊家勝利!");
                         player[j].score -= 2;
                     }
-                    else{
+                    else if (dealer.gameOver) {
                         System.out.println("");
-                        System.out.println("平局!");
-                        player[j].score -= 1;//平局莊家大很正常吧:)//-1分
-                    }
-                }
-            }
-
-            //每一輪結束後顯示玩家當前積分
-            for (int k=0;k<player.length;k++) {
-                System.out.println("玩家"+ (k+1) +"的當前積分: " + player[k].score);
-            }
-
-            rounds++;//輪次+1
-
-            //為了不要讓遊戲無限輪迴，5局強制結束
-            if (rounds >= 5) {
-                System.out.println("遊戲結束!");
-                open = 2; //強制結束遊戲
-            }
-
-            //如果有玩家的積分歸0直接結束
-            //just like日麻被轟飛的概念
-            for (Player p : player) {
-                if (p.score <= 0) {
-                    playGame = false;
-                    break;
-                }
-            }
-            
-            //詢問使用者是否開始第二輪遊戲
-            if (playGame && rounds < 5) {
-                System.out.println("是否繼續下一輪遊戲?");
-                System.out.println("要:1");
-                System.out.println("不要:1以外的數字");
-                while (!sc.hasNextInt()) {
-                    System.out.println("輸入錯誤,請輸入數字:");
-                    sc.next();
-                }
-                int continueGame = sc.nextInt();
-
-                    if(continueGame == 1){
-                        playGame = true;
+                        System.out.println("莊家爆牌，玩家"+(j+1)+"勝利!");
+                        player[j].score += 2;
                     }
                     else {
-                        playGame = false;
-                        //playGame = false會結束遊戲喔注意剛開始的do-while是有條件會結束執行的
-                        //264行
+                        if (player[j].point > dealer.point) {
+                            System.out.println("");
+                            System.out.println("玩家"+(j+1)+"勝利!");
+                            player[j].score += 2;
+                        }
+                        else if (player[j].point < dealer.point) {
+                            System.out.println("");
+                            System.out.println("莊家勝利!");
+                            player[j].score -= 2;
+                        }
+                        else{
+                            System.out.println("");
+                            System.out.println("平局!");
+                            player[j].score -= 1;//平局莊家大很正常吧:)//-1分
+                        }
                     }
-            }
+                }
 
-            }
-            else if(open ==2){
-                System.out.println("真的不玩嗎?");//情緒勒索
-            }
-            else{
-                System.out.println("輸入錯誤,請輸入1或2:");
-            }
-        }while(open !=1 || playGame == true);
+                //每一輪結束後顯示玩家當前積分
+                for (int k=0;k<player.length;k++) {
+                    System.out.println("玩家"+ (k+1) +"的當前積分: " + player[k].score);
+                }
+
+                rounds++;//輪次+1
+                System.out.println("當前回合: "+rounds);
+                //為了不要讓遊戲無限輪迴，5局強制結束
+                if (rounds >= 5) {
+                    System.out.println("遊戲結束!");
+                    playGame = false; //強制結束遊戲
+                }
+
+                //如果有玩家的積分歸0直接結束
+                //just like日麻被轟飛的概念
+                for (Player p : player) {
+                    if (p.score <= 0) {
+                        playGame = false;
+                        break;
+                    }
+                }
+                
+                //詢問使用者是否開始第二輪遊戲
+                if (playGame = true && rounds < 5) {
+                    System.out.println("是否繼續下一輪遊戲?");
+                    System.out.println("要:1");
+                    System.out.println("不要:1以外的數字");
+                    while (!sc.hasNextInt()) {
+                        System.out.println("輸入錯誤,請輸入數字:");
+                        sc.next();
+                    }
+                    int continueGame = sc.nextInt();
+
+                        if(continueGame == 1){
+                            playGame = true;
+                        }
+                        else {
+                            playGame = false;
+                            //playGame = false會結束遊戲喔注意剛開始的do-while是有條件會結束執行的
+                            //264行
+                        }
+                }
+
+                }
+                else if(open ==2){
+                    System.out.println("真的不玩嗎?");//情緒勒索
+                }
+                else{
+                    System.out.println("輸入錯誤,請輸入1或2:");
+                }
+            }while(open !=1);
+        }while (playGame == true);
 
         System.out.println("最終積分如下:");
             for (int k = 0; k < player.length; k++) {
